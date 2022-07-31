a5e7671696f9430c3759fad54fe82ebc6306470e
suji321 <sujisasi4@gmail.com>
2022-07-31 10:57:07 +0530

    switch to active player

--- script.js
+++ script.js
@@ -9,12 +9,17 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 
 // starting conditions
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
+
+const scores = [0, 0]
 let currentScore = 0
+let activePlayer = 0
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,9 +34,13 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice
-        current0El.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
     } else {
         // Switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        currentScore = 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
     }
 })
